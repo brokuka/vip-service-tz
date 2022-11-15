9e1355ce542c4958fe53464eae0b03c6423e0913
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2022-11-15 11:39:42 +0400

    fix pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import AviaPage from "./pages/avia";
-import InfoPage from "./pages/info";
+import InfoPage from "./pages/avia/info";
 
 function App() {
   return (
     <Routes>
-      <Route index path="/avia" element={<AviaPage />} />
+      <Route path="/" element={<AviaPage />} index />
       <Route path="/avia/info" element={<InfoPage />} />
     </Routes>
   );
--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import svgr from "@svgr/rollup";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/avia/",
   plugins: [react(), svgr()],
 });
