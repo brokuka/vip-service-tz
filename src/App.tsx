f1bb1adeab8094273ec228091b486a2cbb9832ec
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2022-11-15 11:52:05 +0400

    avia page default

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import AviaPage from "./pages/avia";
-import InfoPage from "./pages/avia/info";
+import InfoPage from "./pages/info";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<AviaPage />} index />
-      <Route path="/avia/info" element={<InfoPage />} />
+      <Route path="/">
+        <Route element={<Navigate to="/avia" replace />} index />
+        <Route path="/avia" element={<AviaPage />} />
+        <Route path="/avia/info" element={<InfoPage />} />
+      </Route>
     </Routes>
   );
 }
