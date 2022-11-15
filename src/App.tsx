9a8cf7ac33ba4b11b289c217045dd2763db278ac
Ernest Manukyan <erik.manukyan2010@yandex.ru>
2022-11-15 22:55:04 +0400

    index page

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,8 @@ import InfoPage from "./pages/info";
 function App() {
   return (
     <Routes>
-      <Route path="/">
-        <Route element={<Navigate to="/avia" replace />} index />
-        <Route path="/avia" element={<AviaPage />} />
+      <Route>
+        <Route index path="/avia" element={<AviaPage />} />
         <Route path="/avia/info" element={<InfoPage />} />
       </Route>
     </Routes>
